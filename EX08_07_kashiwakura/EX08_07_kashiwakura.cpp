46b8ad30f96e231760fa9f81a2bf3a66ae3cc017
kasshiiii <109583121+kasshiiii@users.noreply.github.com>
2022-12-07 09:38:53 +0900

    20221207

--- EX08_07_kashiwakura/EX08_07_kashiwakura.cpp
+++ EX08_07_kashiwakura/EX08_07_kashiwakura.cpp
@@ -3,25 +3,39 @@ using namespace std;
 
 class Solid {
 public:
-    virtual int GetPackageSize() = 0;
+    virtual void GetPackageSize(int&,int&) = 0;
 };
 
 
 class Package : public Solid {
 private:
     float width, height, depth;
-    int BoxSize[8] = { 60,80,100,120,140,160,180,200 };
+    int kuronekoBoxSize[8] = { 60,80,100,120,140,160,180,200 };
+    int japanpostBoxSize[7] = { 60,80,100,120,140,160,170 };
 public:
     Package(float width, float height, float depth) {
         this->width = width;
         this->height = height;
         this->depth = depth;
     }
-    int GetPackageSize() {
+    void GetPackageSize(int &kuroneko,int &japanpost) {
         float size = width + height + depth;
-        for (int i = 0; i < sizeof BoxSize / sizeof BoxSize[0]; i++) {
-            if (size < BoxSize[i]) {
-                return BoxSize[i];
+        for (int i = 0; i < _countof(kuronekoBoxSize); i++) {
+            if (size <= kuronekoBoxSize[i]) {
+                kuroneko = kuronekoBoxSize[i];
+                break;
+            }
+            else {
+                kuroneko = 0;
+            }
+        }
+        for (int i = 0; i < _countof(japanpostBoxSize); i++) {
+            if (size <= japanpostBoxSize[i]) {
+                japanpost = japanpostBoxSize[i];
+                break;
+            }
+            else {
+                japanpost = 0;
             }
         }
     }
@@ -29,6 +43,7 @@ public:
 int main()
 {
     float width, height, depth;
+    int kuroneko, japanpost;
     cout << "幅を入力してください" << endl;
     cin >> width;
     cout << "高さを入力してください" << endl;
@@ -36,5 +51,17 @@ int main()
     cout << "奥行きを入力してください" << endl;
     cin >> depth;
     Package* package = new Package(width, height, depth);
-    cout << "表面積 = " << package->GetPackageSize() << endl;
+    package->GetPackageSize(kuroneko, japanpost);
+    if (kuroneko == 0) {
+        cout << "クロネコヤマト = 送れません" << "\n日本郵便(ゆうパック) = " << japanpost << endl;
+    }
+    else if (japanpost == 0) {
+        cout << "クロネコヤマト = " << kuroneko << "\n日本郵便(ゆうパック) = 送れません" << endl;
+    }
+    else if (kuroneko == 0 && japanpost == 0) {
+        cout << "クロネコヤマト = 送れません" << "\n日本郵便(ゆうパック) = 送れません" << endl;
+    }
+    else {
+        cout << "クロネコヤマト = " << kuroneko << "\n日本郵便(ゆうパック) = " << japanpost << endl;
+    }
 }
